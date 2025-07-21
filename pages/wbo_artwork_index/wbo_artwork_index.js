bb694dd4a92542c86528ff2f0c20ebd2354d03ee
it-21@1bzimail.com <it-21@1bzimial.com>
2025-07-21 14:58:33 +0800

    更新wbo_artwork_index页面

---
+++ pages/wbo_artwork_index/wbo_artwork_index.js
@@ -0,0 +1,91 @@
+import { Toast } from 'tdesign-miniprogram'; // 轻提示
+import Message from 'tdesign-miniprogram/message/index'; // 提示
+
+Page({
+  data: {
+    // 筛选框变量-1
+    dropdownTemplate: {
+      value: 'all',
+      options: [
+        {
+          value: 'all',
+          label: '全部',
+        },
+        {
+          value: 'HL',
+          label: 'HL',
+        },
+        {
+          value: 'D51',
+          label: 'TG-D51',
+        },
+        {
+          value: 'D240',
+          label: 'TG-D240',
+        },
+
+        {
+          value: 'D234',
+          label: 'TG-D240',
+        },
+      ],
+    },
+    // 筛选框变量-2
+    dropdownSorter: {
+      value: 'default',
+      options: [
+        {
+          value: 'default',
+          label: '默认排序',
+        },
+        {
+          value: 'time',
+          label: '时间从高到低',
+        },
+      ],
+    },
+    searchValue: '',
+  },
+  onChange(e) {
+    this.setData({
+      'product.value': e.detail.value,
+    });
+  },
+  onSearchChange() {
+    console.log(this.data.searchValue);
+  },
+
+  // 跳转到详情页面
+  onJumpArtworkDeatails(e) {
+    const that = this;
+    const groupId =  e.currentTarget.dataset.groupId;
+    wx.showLoading({ title: '正在加载...' });
+    console.log(groupId,"准备跳转");
+    setTimeout(() => {
+      wx.navigateTo({
+        url: `/pages/wbo_artowrk_details/wbo_artowrk_details?groupId=${groupId}`,
+        success(){
+          wx.hideLoading();
+        },
+        fail(err) {
+          Toast({
+            context: that,
+            selector: '#t-toast',
+            message: '跳转失败',
+            theme: 'error',
+            con: 'check-circle',
+          });
+        }
+      });
+    }, 300)
+  },
+
+  // 用户点击右上角分享
+  onShareAppMessage() {
+    return {
+      title: 'WBO',
+      path: 'pages/wbo_artwork_index/wbo_artwork_index',  // 分享后打开的页面路径
+      imageUrl: '/assets/1752927115162.png'     // 自定义分享封面
+    };
+  }
+})
