2decda312854db77137e06f98a2f2781d0b1cb8f
it-21@1bzimail.com <it-21@1bzimial.com>
2025-07-21 16:11:20 +0800

    更新wbo_artwork_details页面

--- pages/wbo_artowrk_details/wbo_artowrk_details.js
+++ pages/wbo_artowrk_details/wbo_artowrk_details.js
@@ -1,18 +1,61 @@
-// pages/wbo_artowrk_details/wbo_artowrk_details.js
+import { Toast } from 'tdesign-miniprogram'; // 轻提示
+import Message from 'tdesign-miniprogram/message/index'; // 提示
+const imageCdn = 'https://tdesign.gtimg.com/mobile/demos';
+const swiperImages = [
+  `${imageCdn}/swiper1.png`,
+  `${imageCdn}/swiper2.png`,
+  `${imageCdn}/swiper1.png`,
+  `${imageCdn}/swiper2.png`,
+  `${imageCdn}/swiper1.png`,
+];
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    groupId: null, // 首页跳转后的存储的id值
+    // 筛选框变量-1
+    dropdownTemplate: {
+      value: 'all',
+      options: [
+        {
+          value: 'all',
+          label: '全部',
+        },
+        {
+          value: 'not_review',
+          label: '未审查',
+        },
+        {
+          value: 'review',
+          label: '已审查',
+        }
+      ],
+    },
+    // 筛选框变量-2
+    dropdownSorter: {
+      value: 'default',
+      options: [
+        {
+          value: 'default',
+          label: '默认排序',
+        },
+        {
+          value: 'time',
+          label: '时间从高到低',
+        },
+      ],
+    },
+    // 轮播图变量
+    current: 0,
+    autoplay: false,
+    duration: 500,
+    interval: 5000,
+    swiperImages,
+    cur: {},
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const groupId = options.groupId; // 首页跳转后的存储的id值
   },
 
   /**
@@ -62,5 +105,19 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+
+  // 轮播图函数
+  onTap(e) {
+    const { index } = e.detail;
+    console.log(index);
+  },
+  onChange(e) {
+    const { current, source } = e.detail;
+
+    console.log(current, source);
+  },
+  onImageLoad(e) {
+    console.log(e.detail.index);
+  },
 })
