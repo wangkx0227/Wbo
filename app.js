500dd08fbc7153aea0e079a119f2d98a98db4f5b
不会换牙的鲨鱼 <wangkx0227@163.com>
2025-08-05 14:35:31 +0800

    更新

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ App({
     token: null,       // token
     userRole: null,     // 用户角色，比如 'kyle'
     userInfo: null,   // 当前登录用户信息
-    url: 'https://xcx.1bizmail.com:8153/', // 后端接口路径
+    url: 'https://xcx.1bizmail.com:8153/api/', // 后端接口路径
   },
   onLaunch(options) {
     // 小程序初始化（只执行一次）
