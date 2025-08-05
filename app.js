ff3c5a2061bdc2b8691d5594c8bd52de7d1f5f77
不会换牙的鲨鱼 <wangkx0227@163.com>
2025-08-05 13:23:53 +0800

    更新

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-// app.js
 App({
-  globalData: {
+  globalData: { // 全局变量
+    token: null,       // token
+    userRole: null,     // 用户角色，比如 'kyle'
     userInfo: null,   // 当前登录用户信息
-    userRole: '',     // 用户角色，比如 'kyle' | 'shelley' | 'fmr' | 或者其他
-    token: ''         // 其他全局信息
+    url: 'https://xcx.1bizmail.com:8153/', // 后端接口路径
   },
-  onLaunch() {
-    // 展示本地存储能力
-    console.log("小程序启动");
-    // 每次启动重置登录
-    wx.setStorageSync('userRole', ''); 
-    // const logs = wx.getStorageSync('logs') || []
-    // console.log(logs);
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
-
-    // // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //   }
-    // })
+  onLaunch(options) {
+    // 小程序初始化（只执行一次）
+    if (!wx.getStorageSync('firstLaunchTime')) {
+      wx.setStorageSync('firstLaunchTime', Date.now())
+    }
   },
-
 })
