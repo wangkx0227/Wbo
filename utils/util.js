c45fadd39376a9d3b3768195eda132dadd97d9eb
不会换牙的鲨鱼 <wangkx0227@163.com>
2025-08-06 11:28:17 +0800

    更新

--- utils/util.js
+++ utils/util.js
@@ -24,7 +24,26 @@ function showToast(that,content,theme="success") {
     direction: 'row',
   });
 }
-
+onSwiperImagesTap(e) {
+  const { index } = e.detail;
+  const that = this;
+  // const urls = e.currentTarget.dataset.images || [];   // 所有图片对象数组
+  // const index = e.currentTarget.dataset.index;         // 当前图片索引
+  // //把图片对象数组提取成 URL 数组
+  // const urlList = urls.map(img => img.image_url);
+  const swiperImages = this.data.swiperImages; // 假数据
+  const current = swiperImages[index];
+  if (!current || !swiperImages.length) {
+    const theme = "error"
+    const message = "无法预览图片"
+    utils.showToast(that, message, theme);
+    return;
+  }
+  wx.previewImage({
+    current,
+    urls: swiperImages
+  });
+}
 module.exports = {
   formatTime,
   showToast
