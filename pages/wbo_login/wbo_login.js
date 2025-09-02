195656120344b132936e0c9a9e9103a965ff2ae5
不会换牙的鲨鱼 <wangkx0227@163.com>
2025-09-02 14:34:38 +0800

    更新

--- pages/wbo_login/wbo_login.js
+++ pages/wbo_login/wbo_login.js
@@ -256,10 +256,11 @@ Page({
       } else if (productValue === "fmr") {
         wx.setStorageSync('userName', "刘开波"); // 样品照片拍照上传 fmr 测试 （fmr系列测试）fmr可行性分析
         wx.setStorageSync('apiUserName', "lkb"); // 访问接口携带名字
+        wx.setStorageSync('position_list', ["FMR"]); // 访问接口携带名字
       } else if (productValue === "designer") {
-        wx.setStorageSync('userName', "Ethan"); // 样品照片拍照审核  设计师 测试 上传ait稿与工厂稿和照片审核
-        wx.setStorageSync('apiUserName', "Ethan"); // 访问接口携带名字
-        wx.setStorageSync('position_list', ["设计经理"]); // 访问接口携带名字
+        wx.setStorageSync('userName', "黄思杰"); // 样品照片拍照审核  设计师 测试 上传ait稿与工厂稿和照片审核
+        wx.setStorageSync('apiUserName', "hsj"); // 访问接口携带名字
+        wx.setStorageSync('position_list', ["AIT"]); // 访问接口携带名字
       } else if (productValue === "chosen_draft") { // 第7轮与第9轮
         wx.setStorageSync('userName', "kyle");
         wx.setStorageSync('apiUserName', "kyle"); // 访问接口携带名字
